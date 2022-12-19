8f005a35c613d6e777bfe12841f91af186b75227
andredcp <andre.dacostap3@gmail.com>
2022-12-20 00:11:14 +0100

    fist commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import * as dotenv from "dotenv";
+
+
+dotenv.config();
+const app = express();
+
+
+app.use(express.json());
+
+app.listen(Number(process.env.PORT), () => {
+    console.log(`Server up and running at port ${process.env.PORT}`);
+});
