b7cc84dd9300c7dce67fd9eba33527b96a881c35
andredcp <andre.dacostap3@gmail.com>
2023-01-04 00:59:14 +0100

    t2

---
+++ src/config/aws3.config.ts
@@ -0,0 +1,26 @@
+const aws = require("aws-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+
+const s3 = new aws.S3({
+  accessKeyId: process.env.S3_ACCESS_KEY,
+  secretAccessKey: process.env.S3_ACCESS_SECRET,
+  region: process.env.S3_DEFAULT_REGION,
+});
+
+const uploadS3 = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.AWS_S3_BUCKET,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: "public-read",
+    metadata: (req: any, file: any, cb: any) => {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: (req: any, file: any, cb: any) => {
+      cb(null, "files_from_node/" + Date.now().toString() + file.originalname);
+    },
+  }),
+});
+
+export default uploadS3;
--- src/routes/uploadImg.route.ts
+++ src/routes/uploadImg.route.ts
@@ -1,18 +1,21 @@
 import express from "express";
-import { uploadImgMulter } from "../config/cloudinary.config";
+import uploadS3 from "../config/aws3.config";
 
 const uploadImageRouter = express.Router();
 
 uploadImageRouter.post(
-  "/",
-  uploadImgMulter.single("picture"),
+  "/upload",
+  uploadS3.array("inputFile", 3),
   (req: any, res: any) => {
     if (!req.file) {
       console.log(req.file);
       return res.status(400).json({ msg: "Upload fail" });
     }
 
-    return res.status(201).json({ url: req.file.path });
+    return res.status(201).json({
+      message: "Successfully uploaded " + req.files.length + " files!",
+      files: req.files,
+    });
   }
 );
 
